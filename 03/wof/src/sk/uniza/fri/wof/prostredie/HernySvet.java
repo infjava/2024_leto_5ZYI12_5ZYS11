b2ea23ec8890d7d9fbe0d953de7ee343dc5a67ef
Jan Janech <jan.janech@fri.uniza.sk>
2025-03-05 08:31:18 +0100

    03 verzia WOF z moodle

---
+++ 03/wof/src/sk/uniza/fri/wof/prostredie/HernySvet.java
@@ -0,0 +1,40 @@
+package sk.uniza.fri.wof.prostredie;
+
+import sk.uniza.fri.wof.prostredie.predmety.Hodinky;
+import sk.uniza.fri.wof.prostredie.predmety.Navleky;
+import sk.uniza.fri.wof.prostredie.predmety.ObycajnyPredmet;
+import sk.uniza.fri.wof.prostredie.vychody.VstupDoLabaku;
+
+public class HernySvet {
+    private final Miestnost startovaciaMiestnost;
+
+    public HernySvet() {
+        Miestnost terasa = new Miestnost("terasa - hlavny vstup na fakultu");
+        Miestnost aula = new Miestnost("aula");
+        Miestnost bufet = new Miestnost("bufet");
+        Miestnost labak = new Miestnost("pocitacove laboratorium");
+        Miestnost kancelaria = new Miestnost("kancelaria spravcu pocitacoveho laboratoria");
+
+        // inicializacia miestnosti = nastavenie vychodov
+        terasa.nastavVychod("vychod", aula);
+        terasa.nastavVychod("juh", new VstupDoLabaku(labak));
+        terasa.nastavVychod("zapad", bufet);
+        aula.nastavVychod("zapad", terasa);
+        labak.nastavVychod("sever", terasa);
+        labak.nastavVychod("vychod", kancelaria);
+        kancelaria.nastavVychod("zapad", labak);
+        bufet.nastavVychod("vychod", terasa);
+
+        terasa.polozPredmet(new ObycajnyPredmet("kamen"));
+        terasa.polozPredmet(new Hodinky());
+        terasa.polozPredmet(new Navleky());
+        labak.polozPredmet(new ObycajnyPredmet("mys"));
+        bufet.polozPredmet(new ObycajnyPredmet("bageta"));
+
+        this.startovaciaMiestnost = terasa;  // startovacia miestnost hry
+    }
+
+    public Miestnost getStartovaciaMiestnost() {
+        return this.startovaciaMiestnost;
+    }
+}
---
+++ 03/wof/src/sk/uniza/fri/wof/prostredie/vychody/VstupDoLabaku.java
@@ -0,0 +1,23 @@
+package sk.uniza.fri.wof.prostredie.vychody;
+
+import sk.uniza.fri.wof.prostredie.Miestnost;
+import sk.uniza.fri.wof.zaklad.Hrac;
+
+public class VstupDoLabaku implements Vychod {
+    private final Miestnost miestnost;
+
+    public VstupDoLabaku(Miestnost miestnost) {
+        this.miestnost = miestnost;
+    }
+
+    @Override
+    public Miestnost dajMiestnost() {
+        return this.miestnost;
+    }
+
+    @Override
+    public boolean mozemVstupit(Hrac hrac) {
+        var navleky = hrac.najdiPredmet("navleky");
+        return navleky.isPresent() && navleky.get().jeNasadeny();
+    }
+}
