ab74fbc2c90eac86bd523153c9973847126aee8c
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-16 08:52:48 +0200

    09 zakladny navrh okna

--- 09/vtip/src/Main.java
+++ 09/vtip/src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var okno = new JFrame("Dôležitá otázka");
+
+        okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        okno.setLayout(new GridLayout(2, 2));
+
+        var popisok = new JLabel("Chceš úspešne absolvovať predmet Informatika 2?");
+
+        var anoTlacitko = new JButton("áno");
+        var nieTlacitko = new JButton("nie");
+
+        okno.add(popisok);
+        okno.add(new JLabel());
+        okno.add(anoTlacitko);
+        okno.add(nieTlacitko);
+
+        okno.pack();
+
+        okno.setVisible(true);
     }
 }
