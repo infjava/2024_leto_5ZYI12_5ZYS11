856433fef6093b07a4325ce69818c37ad6a13dae
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-30 08:36:40 +0200

    10 zobrazenie okna pre vyber suboru a otvorenie suboru na zapis

--- 09/osoby/src/ZoznamStudentov.java
+++ 09/osoby/src/ZoznamStudentov.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ZoznamStudentov {
     private final JFrame okno;
@@ -52,7 +56,18 @@ public class ZoznamStudentov {
     }
 
     private void ulozitDoSuboru() {
+        var vyberSuboru = new JFileChooser();
 
+        var vysledok = vyberSuboru.showSaveDialog(this.okno);
+
+        if (vysledok == JFileChooser.APPROVE_OPTION) {
+            var subor = vyberSuboru.getSelectedFile();
+            try (var zapisovac = new ObjectOutputStream(new FileOutputStream(subor))) {
+
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this.okno, "Nepodarilo sa ulozit");
+            }
+        }
     }
 
     private void vlastnostiSaZmenili() {
