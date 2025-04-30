21fbeac822c8a2101d3aa6bc5dae7932056524ad
Jan Janech <jan.janech@fri.uniza.sk>
2025-04-30 10:10:09 +0200

    10 windows vzhlad

--- 09/osoby/src/Main.java
+++ 09/osoby/src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+
+        }
+
         var okno = new ZoznamStudentov();
         okno.zobraz();
     }
